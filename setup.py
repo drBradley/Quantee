4bcb8dcb1fcc7ca5126488891712165f7ba16978
Karol Marcjan <karol.marcjan@gmail.com>
2012-12-12 12:36:47 +0100

    Start work on the build process

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='quantee',
+      version='0.0.3',
+      package_dir={'quantee': 'src'},
+      packages=['quantee'])
