21913be69e2a2077c8e758883bda8b0d8755771c
Karol Marcjan <karol.marcjan@gmail.com>
2013-01-25 10:32:58 +0100

    Stop DumbDirector from toggling fullscreen. It got annoying.

--- src/quantee.py
+++ src/quantee.py
@@ -222,10 +222,6 @@ class DumbDirector(Director):
 
         self.__box = box
 
-        self.__toggle_fullscreen_each_ms = toggle_fullscreen_each_s * 1000
-        self.__time = 0
-        self.__last_toggle = 0
-
     def hints(self, entity):
 
         return None
@@ -242,18 +238,6 @@ class DumbDirector(Director):
 
             for i in range(len(levels)):
                 levels.pop()
-
-        # Handle toggling between fullscreen and windowed mode each n seconds
-        self.__time += dt
-
-        toggle_no = self.__time / self.__toggle_fullscreen_each_ms
-
-        if toggle_no > self.__last_toggle:
-
-            options.set_fullscreen(bool(toggle_no % 2))
-            options.confirm()
-
-            self.__last_toggle += 1
 
     def viewport(self, stage):
 
